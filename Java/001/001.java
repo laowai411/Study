c6f61b26ba6d7130f4efb279800bd89656f0a6f8
baoziyyy <530923110@qq.com>
2014-12-04 22:55:46 +0800

    commit taskoo1

--- Java/001/001.java
+++ Java/001/001.java
@@ -3,11 +3,26 @@ package flower;
 public class daffodil {
 
 	public static void main(String[] args) {
-		int s = 0;
-	    for (int i = 1; i <= 1000; ++i)
-	      s += i;
-	    System.out.println(s);
-	}
-	
+	      System.out.println("水仙花数为：");
+
+        int a = 0, b = 0, c = 0;
+
+        for(int i = 100; i < 1000; i++){
+
+            a = (i / 100) % 10;
+
+            b = (i / 10) % 10;
+
+            c = i % 10;
+
+            if(i == a*a*a + b*b*b + c*c*c){
+
+                System.out.print(i + " ");
+
+            }
+
+        }
+
+    }
 
 }
--- Java/001/003.java
+++ Java/001/003.java
@@ -1,12 +1,8 @@
-package multiplication;
-
-public class multiplication_2 {
-
+public class multiplicaiton_1{ 
 	public static void main(String[] args)  {   
-		for (int i = 9;  i >= 1; i--)    {      
-			for(int n = i;  n > 0 & n <= i; n--)      {        
-				System.out.print( i + " x " + n + " = " + i * n + " ");    	
-			}  
+		for (int i = 1;  i <= 9; i++)    {      
+			for(int n = 1; n <= i; n++)      {        
+				System.out.print( i + " x " + n + " = " + i * n + " ");      }      
 			System.out.println();  
 			}
 		}
